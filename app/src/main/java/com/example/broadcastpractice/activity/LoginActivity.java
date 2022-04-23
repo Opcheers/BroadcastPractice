78fcfeba86bf810119fc79380431ffaa2331e515
Opcheers <yxq9902@163.com>
2022-04-23 21:44:46 +0800

    broadcast+SharedPreferences存储

--- app/src/main/java/com/example/broadcastpractice/activity/LoginActivity.java
+++ app/src/main/java/com/example/broadcastpractice/activity/LoginActivity.java
@@ -1,9 +1,13 @@
 package com.example.broadcastpractice.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,13 +22,15 @@ public class LoginActivity extends BaseActivity {
     private EditText mPwdEt;
     private EditText mAccountEt;
     private Button mLoginBtn;
+    private CheckBox mCheckBox;
+    private SharedPreferences pref;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        initview();
+        initView();
         initEvent();
     }
 
@@ -34,6 +40,19 @@ public class LoginActivity extends BaseActivity {
             public void onClick(View view) {
                 String account = mAccountEt.getText().toString().trim();
                 String password = mPwdEt.getText().toString().trim();
+
+                //数据存储
+                SharedPreferences.Editor editor = pref.edit();
+                if (mCheckBox.isChecked()) {
+                    editor.putString("account", account);
+                    editor.putString("password", password);
+                    editor.putBoolean("remember", true);
+                } else {
+                    editor.clear();
+                }
+                editor.apply();
+
+                //登录账号密码校验
                 if (account.equals("123456") && password.equals("123456")){
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
@@ -45,10 +64,23 @@ public class LoginActivity extends BaseActivity {
         });
     }
 
-    private void initview() {
+    private void initView() {
+
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
 
         mLoginBtn = this.findViewById(R.id.login);
         mAccountEt = this.findViewById(R.id.account);
         mPwdEt = this.findViewById(R.id.password);
+        mCheckBox = this.findViewById(R.id.remember_pwd);
+
+        //第一次使用时肯定没有被选中，所以默认值为fasle
+        boolean isRemember = pref.getBoolean("remember", false);
+
+        //如果被选中，则读取数据显示到页面上
+        if (isRemember) {
+            mAccountEt.setText(pref.getString("account", ""));
+            mPwdEt.setText(pref.getString("password", ""));
+            mCheckBox.setChecked(true);
+        }
     }
 }
